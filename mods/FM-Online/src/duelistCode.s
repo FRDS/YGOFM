41e924bdaea2464ed3abb08cfe61e5f7857cd78f
Agung Firdaus <agung.frds7@gmail.com>
2023-10-30 18:03:06 +0700

    FMOnline almost done

---
+++ mods/FM-Online/src/duelistCode.s
@@ -0,0 +1,4 @@
+.set noreorder
+nop
+j 0x8003fcc8
+li v0, 1
--- mods/FM-Online/src/fmonline_main.c
+++ mods/FM-Online/src/fmonline_main.c
@@ -1,147 +1,125 @@
 #include <common.h>
 
+/* ASM Locations */
+u_int *maxLPDigits[2] = {0x80016F98, 0x80016F14};
+u_int *playerBeginMatch = 0x80017AF2;
+u_int *triangleText = 0x800284D8;
+u_int *triangleImage = 0x8002946C;
+u_int *playGsSound = 0x800370E4;
+u_int *drawGsCursor[3] = {0x800370EC, 0x800371D0, 0x8003725C};
+u_int *drawGsSymbols = 0x80037FF4;
+u_int *drawGsText = 0x80039730;
+u_char *lpDigits[2] = {0x80016F98, 0x80016F14};
+
+/* ASM Instructions */
+int triangleText = 0x93860343;
+int triangleImage = 0x24060004;
+int playSound = 0x0C00FFB8;
+int drawGsCursor[3] = {0x0C00DBE0, 0x0C01001B, 0x0C00DBE0};
+int drawText = 0x0C00DB05;
+
+char i, duelEnded, playing, changeTurn, switchedSides = 0;
+char p1Begin = 1;
+
 void ChangeCardView(char player)
 {
-    sdata->DuelScreen[player].viewCard = 0;
-    sdata->DuelScreen[player % 2].viewCard = -1;
+	sdata->DuelScreen[player].viewCard = 0;
+	sdata->DuelScreen[player + 1 % 2].viewCard = -1;
 }
 
 void AdjustLPString()
 {
-	for (int i = 0; i < numPlayers; i++)
+	for (i = 0; i < 2; i++)
 	{
-		ReadMem(RAM::lp[i] - 2, &lp[i], sizeof(lp[i]));
-		if (lp[i] > 9999)
-			WriteMem(RAM::maxLPDigits[i], &Cheat::maxLPDigits, sizeof(Cheat::maxLPDigits));
+		if (sdata->DuelScreen[i].LPActual > 9999)
+			*lpDigits[i] = 5;
 		else
-			WriteMem(RAM::maxLPDigits[i], &Cheat::minLPDigits, sizeof(Cheat::minLPDigits));
+			*lpDigits[i] = 4;
 	}
-}
-
-void HookDuelistCodeBypass()
-{
-	WriteMem(RAM::duelistCode, &Cheat::nop, sizeof(Cheat::nop));
-	WriteMem(RAM::duelistCodeRet, &Cheat::duelistCodeRet, sizeof(Cheat::duelistCodeRet));
-}
-
-char SceneIsPvP()
-{
-	ReadMem(RAM::currentScene, &currentScene, sizeof(currentScene));
-	return currentScene == PvPScene;
-}
-
-char SceneIsDuel()
-{
-	ReadMem(RAM::currentScene, &currentScene, sizeof(currentScene));
-	return currentScene == duelScene;
-}
-
-char TurnTransitioned()
-{
-	ReadMem(RAM::currentTurn, &currentTurn, sizeof(currentTurn));
-
-	if (currentTurn != lastTurn)
-	{
-		lastTurn = currentTurn;
-		return true;
-	}
-	return false;
-}
-
-void SetSeed()
-{
-	ReadMem(RAM::prng, &seed, sizeof(seed));
-	cout << "\nSeed: 0x" << hex << seed << ". Make sure that your seed is synchronized with your opponent's starting seed." << endl;
 }
 
 void AdvanceSeed()
 {
-	for (int i = 0; i < 255; i++)
+	int seed = sdata->prngSeed;
+
+	for (i = 0; i < 255; i++)
 		seed = (seed * 0x41C64E6D) + 0x3039; // LCG using FM's params
 
-	WriteMem(RAM::prng, &seed, sizeof(seed));
-}
-
-void RemoveTrapRotation()
-{
-	for (int i = 0; i < numTrapRotInst; ++i)
-		WriteMem(RAM::trapRot[i], &Cheat::nop, sizeof(Cheat::nop));
+	sdata->prngSeed = seed;
 }
 
 void SwitchSides()
 {
-	if (SceneIsPvP())
+	if (sdata->currentScene == DUEL_SETUP)
 	{
 		if (!switchedSides)
 		{
-			switchedSides = true;
-			duelEnded = false;
+			switchedSides = 1;
+			duelEnded = 0;
 			AdvanceSeed();
 			if (p1Begin)
 			{
-				WriteMem(RAM::playerBeginMatch, &Cheat::playerBeginMatch[1], sizeof(Cheat::playerBeginMatch[1]));
-				p1Begin = false;
+				*playerBeginMatch = 0x90;
+				p1Begin = 0;
 			}
 			else
 			{
-				WriteMem(RAM::playerBeginMatch, &Cheat::playerBeginMatch[0], sizeof(Cheat::playerBeginMatch[0]));
-				p1Begin = true;
+				*playerBeginMatch = 0x80;
+				p1Begin = 1;
 			}
 		}
 	}
 	else
 	{
-		switchedSides = false;
+		switchedSides = 0;
 	}
 }
 
 void UpdateImgAndText(char player)
 {
-	if (player == currentTurn)
+	if (player == sdata->currentTurn)
 	{
-		WriteMem(RAM::playGsSound, &Cheat::playSound, sizeof(Cheat::playSound));
-		for (int i = 0; i < numDrawCursorInst; ++i)
-			WriteMem(RAM::drawGsCursor[i], &Cheat::drawGsCursor[i], sizeof(Cheat::drawGsCursor[i]));
-		WriteMem(RAM::drawGsSymbols, &Cheat::drawText, sizeof(Cheat::drawText));
-		WriteMem(RAM::drawGsText, &Cheat::drawText, sizeof(Cheat::drawText));
-		WriteMem(RAM::triangleImage, &Cheat::triangleImage, sizeof(Cheat::triangleImage));
-		WriteMem(RAM::triangleText, &Cheat::triangleText, sizeof(Cheat::triangleText));
+		*playGsSound = playGsSound;
+		for (i = 0; i < 3; ++i)
+			*drawGsCursor[i] = drawGsCursor;
+		*drawGsSymbols = drawGsSymbols;
+		*drawGsText = drawGsText;
+		*triangleImage = triangleImage;
+		*triangleText = triangleText;
 	}
 	else
 	{
-		WriteMem(RAM::playGsSound, &Cheat::nop, sizeof(Cheat::nop));
-		for (int i = 0; i < numDrawCursorInst; ++i)
-			WriteMem(RAM::drawGsCursor[i], &Cheat::nop, sizeof(Cheat::nop));
-		WriteMem(RAM::drawGsSymbols, &Cheat::nop, sizeof(Cheat::nop));
-		WriteMem(RAM::drawGsText, &Cheat::nop, sizeof(Cheat::nop));
-		WriteMem(RAM::triangleImage, &Cheat::nop, sizeof(Cheat::nop));
-		WriteMem(RAM::triangleText, &Cheat::nop, sizeof(Cheat::nop));
+		*playGsSound = 0;
+		for (i = 0; i < 3; ++i)
+			*drawGsCursor[i] = 0;
+		*drawGsSymbols = 0;
+		*drawGsText = 0;
+		*triangleImage = 0;
+		*triangleText = 0;
 	}
 }
 
 char LoadingLifePoints()
 {
-	char loading = false;
-	for (int i = 0; i < numPlayers; ++i)
+	char loading = 0;
+	for (int i = 0; i < 2; ++i)
 	{
-		ReadMem(RAM::lp[i], &lp[i], sizeof(lp[i]));
-		if (!lp[i])
-			loading = true;
+		if (!sdata->DuelScreen[i].LPActual)
+			loading = 1;
 	}
 	return loading;
 }
 
 void CheckEndOfDuel()
 {
-	char ended = false;
-	for (int i = 0; i < numPlayers; ++i)
+	char ended = 0;
+	for (int i = 0; i < 2; ++i)
 	{
-		ReadMem(RAM::lp[i], &lp[i], sizeof(lp[i]));
-		if (!lp[i])
-			ended = true;
+		if (!sdata->DuelScreen[i].LPActual)
+			ended = 1;
 	}
 	if (ended)
-		UpdateImgAndText(currentTurn);
+		UpdateImgAndText(sdata->currentTurn);
 }
 
 void HandleGame(char player)
@@ -152,20 +130,20 @@ void HandleGame(char player)
 	}
 	else
 	{
-		if (SceneIsDuel())
+		if (sdata->currentScene == DUEL_2P)
 		{
 			ChangeCardView(player);
+
 			if (!playing)
 			{
 				while (LoadingLifePoints())
 					ChangeCardView(player);
-				playing = true;
-				TurnTransitioned();
+				playing = 1;
 				UpdateImgAndText(player);
 			}
 
 			AdjustLPString();
-			if (TurnTransitioned())
+			if (sdata->currentTurn != player)
 				UpdateImgAndText(player);
 
 			CheckEndOfDuel();
@@ -174,10 +152,24 @@ void HandleGame(char player)
 		{
 			if (playing)
 			{
-				duelEnded = true;
-				playing = false;
-				UpdateImgAndText(currentTurn);
+				duelEnded = 1;
+				playing = 0;
+				UpdateImgAndText(sdata->currentTurn);
 			}
 		}
 	}
+}
+
+// Still trying to find where they process inputs to determine global player.
+char DeterminePlayer()
+{
+}
+
+// Hook into a function that runs every frame, Vsync()?
+void fmOnline_main()
+{
+	if (sdata->currentScene != DUEL_SETUP && sdata->currentScene != DUEL_2P)
+		return;
+	char player = DeterminePlayer();
+	HandleGame(player);
 }
